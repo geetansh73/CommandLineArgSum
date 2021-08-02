162394028a39ce409681e85a5b24d38536f74b53
Geetansh Gupta <67698473+geetansh73@users.noreply.github.com>
2021-08-02 22:52:06 +0530

    Create cmdLineArgsum.java

---
+++ cmdLineArgsum.java
@@ -0,0 +1,10 @@
+class commandlineargsum
+{
+public static void main(String geetansh[])
+{
+int x=Integer.parseInt(geetansh[0]);
+int y=Integer.parseInt(geetansh[1]);
+int z=x+y;
+System.out.print(z);
+}
+}
